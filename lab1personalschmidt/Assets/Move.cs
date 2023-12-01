dd60073d567255d34396712cd9c35975f283930d
Jcvxka <jcvxka@gmail.com>
2023-12-01 00:15:47 -0800

    went to vectors done

---
+++ lab1personalschmidt/Assets/Move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    public float speed = 5.0f;
+
+    void Start() {
+
+        //direction = goal.transform.position - transform.position;
+        //this.transform.Translate(direction);
+    }
+
+    private void LateUpdate() {
+        direction = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if(direction.magnitude > 2)
+        {
+            Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+        
+    }
+}
